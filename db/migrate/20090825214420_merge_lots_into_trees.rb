7b340012c8e31ee5219408d561be06928e09d159
Tim Diggins <tim@red56.co.uk>
2009-08-31 14:06:21 +0100

    restored full migration

--- db/migrate/20090825214420_merge_lots_into_trees.rb
+++ db/migrate/20090825214420_merge_lots_into_trees.rb
@@ -14,30 +14,30 @@ end
 
 class MergeLotsIntoTrees < ActiveRecord::Migration
   def self.up
-#    add_column :trees, :nearest_address, :string
-#    add_column :trees, :how_to_find, :string
-#    add_column :trees, :postcode, :string
-#    add_column :trees, :lat, :float
-#    add_column :trees, :lng, :float
-#    add_column :comments, :tree_id, :integer
-#    
-#    Lot.all.each do |lot|
-#      puts "lot: #{lot} (tree: #{lot.tree})"
-#      tree = lot.tree || Tree.new
-#      tree.nearest_address = lot.nearest_address
-#      tree.how_to_find = lot.how_to_find
-#      tree.postcode = lot.postcode
-#      tree.lat = lot.lat
-#      tree.lng = lot.lng
-#      tree.save!
-#      
-#      lot.comments.each do |c| 
-#        tree.comments << c
-#        c.save!
-#      end
-#    end
+    add_column :trees, :nearest_address, :string
+    add_column :trees, :how_to_find, :string
+    add_column :trees, :postcode, :string
+    add_column :trees, :lat, :float
+    add_column :trees, :lng, :float
+    add_column :comments, :tree_id, :integer
     
-#    remove_column :comments, :lot_id
+    Lot.all.each do |lot|
+      puts "lot: #{lot} (tree: #{lot.tree})"
+      tree = lot.tree || Tree.new
+      tree.nearest_address = lot.nearest_address
+      tree.how_to_find = lot.how_to_find
+      tree.postcode = lot.postcode
+      tree.lat = lot.lat
+      tree.lng = lot.lng
+      tree.save!
+      
+      lot.comments.each do |c| 
+        tree.comments << c
+        c.save!
+      end
+    end
+    
+    remove_column :comments, :lot_id
     drop_table :lots
   end
   
